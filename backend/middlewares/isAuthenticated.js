2e39914e2941aa359d11085242af74676ff11d70
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-11-27 19:23:29 +0530

    Initial commit

---
+++ backend/middlewares/isAuthenticated.js
@@ -0,0 +1,16 @@
+function isAuthenticated(req, res, next) {
+  try {
+    if (req.session.username && req.session.password) {
+      next();
+    } else {
+      console.log(req.session.username, req.session.password);
+      throw new Error("Not authorized");
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = isAuthenticated;
+
+/* eslint linebreak-style: ["error", "windows"] */
