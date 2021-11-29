ec57d0bf94db00624d1c4bf5ecf4c892b072265c
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-11-29 22:39:19 +0530

    Initial commit

---
+++ frontend/src/assets/js/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
